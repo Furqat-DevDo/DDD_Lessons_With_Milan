b7a3619532d2d78737e4f9af04cf9d0cf42cacb7
Furqat <89734235+Furqat-DevDo@users.noreply.github.com>
2024-01-23 16:03:59 +0500

    Domain Modeling First Lesson

---
+++ Firts_Lesson/Domain/Orders/Order.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Customers;
+using Domain.Products;
+
+namespace Domain.Orders;
+
+public class Order
+{
+    private readonly HashSet<LineItem> _items = new();
+    private Order()
+    {
+        
+    }
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+    public List<LineItem> Items { get; private set; }
+
+    public static Order Craete(Customer customer)
+    {
+        var order = new Order
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = customer.Id
+        };
+        
+        return order;
+    }
+    
+    public void Add(Product product)
+    {
+        var lineItem = new LineItem(Guid.NewGuid(), Id, product.Id, product.Price);
+        _items.Add(lineItem);
+    }
+}
